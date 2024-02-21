6fc58daba7267b5796c616e509a152427da11f7c
Atharva Yadav <111379708+BillyBubbles15@users.noreply.github.com>
2024-02-22 01:49:10 +1300

    Add files via upload

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
         const email=localStorage.getItem('email')
        
         const response = await axios.get(`http://localhost:9090/bridge/showbridge?email=${email}`);
-        if (response.status === 200) {
+        if (response.status >= 200 && response.status < 300) {
           console.log(response.data);
           setBackEndData(response.data);         
         } else {
@@ -58,11 +58,11 @@ const Home = () => {
         </div>
         <div className='text-center w-1/2 pr-6'>
           <h1 className='text-4xl font-bold text-pink-600 my-12'>REGISTER</h1>
-          <h1 className='text-white font-sm mb-6 text-l cursor-pointer mx-56 bg-black px-3 py-2 rounded-sm hover:bg-pink-600' onClick={Drop}>Select a method to Enter Data &nbsp;&nbsp; <IoIosArrowDropdown style={{alignItems: 'center' , display:'inline-flex'}} size={24}/></h1>
+          <h1 className='text-white font-sm mb-6 text-l cursor-pointer mx-48 bg-black px-3 py-2 rounded-sm hover:bg-pink-600' onClick={Drop}>Select a method to Enter Data &nbsp;&nbsp; <IoIosArrowDropdown style={{alignItems: 'center' , display:'inline-flex'}} size={24}/></h1>
           {Dropdown && (
-          <div className='grid justify-center'>
+          <div className='flex justify-center'>
             <button onClick={addbridge} className='bg-pink-600 px-3 mx-5 mb-5 py-2 text-gray-100 rounded-sm hover:bg-black'>Add Manually</button>
-            <button onClick={addcsv} className='bg-pink-600 px-4 py-2 mx-5 text-gray-100 rounded-sm hover:bg-black'>Upload Excel</button>
+            <button onClick={addcsv} className='bg-pink-600 px-3 py-2 mx-5 mb-5 text-gray-100 rounded-sm hover:bg-black'>Upload Excel</button>
           </div>
           )}
 
